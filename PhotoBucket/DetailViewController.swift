4148fd3b3ef89d06052d581e5759dbabe0cc5ccf
Christopher Robert Samp <sampcr@rose-hulman.edu>
2018-04-18 13:03:06 -0400

    finished PhotoBucket lab

--- PhotoBucket/DetailViewController.swift
+++ PhotoBucket/DetailViewController.swift
@@ -12,30 +12,75 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    var photo: Photo?
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.timestamp!.description
-            }
-        }
+        captionLabel.text = photo?.caption
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(showEditDialog))
+        
         configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func showEditDialog() {
+        let alertController = UIAlertController(title: "Edit Caption", message: "", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "caption"
+            textField.text = self.photo?.caption
+        }
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        let createQuotesAction = UIAlertAction(title: "edit", style: .default) { (action) in
+            let captionTextField = alertController.textFields![0]
+            self.photo?.caption = captionTextField.text!
+            self.configureView()
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(createQuotesAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 
-    var detailItem: Event? {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.captionLabel.text = self.photo?.caption
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("int view did appear")
+        if let imgString = photo?.url {
+            if let imgUrl = URL(string: imgString) {
+                DispatchQueue.global().async { // Download in the background
+                    do {
+                        let data = try Data(contentsOf: imgUrl)
+                        DispatchQueue.main.async { // Then update on main thread
+                            print("updated the image")
+                            self.image.image = UIImage(data: data)
+                        }
+                    } catch {
+                        print("Error downloading image: \(error)")
+                    }
+                }
+            }
+        }
+    }
+
+
+    var detailItem: Photo? {
         didSet {
-            // Update the view.
+            captionLabel.text = photo?.caption
             configureView()
         }
     }
--- PhotoBucket/MasterViewController.swift
+++ PhotoBucket/MasterViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 import CoreData
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
+    let photoBucketCellIdentifier = "PhotoBucketCell"
+    let showDetailSegueIdentifier = "ShowDetailSegue"
+    let names = ["Fire!", "Blue Lightning", "Wow!", "Lightning"]
+    var photoBuckets = [Photo]()
 
 
     override func viewDidLoad() {
@@ -26,171 +32,120 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+//        let photo1:Photo = Photo(context: self.context)
+//        photo1.created = Date()
+//        photo1.caption = "Fire!"
+//        photo1.url = "abc.com"
+//        photoBuckets.append(photo1)
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
+        self.updatePhotoBucketArray()
+        tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
     @objc
     func insertNewObject(_ sender: Any) {
-        let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-             
-        // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
-
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        let alertController = UIAlertController(title: "Create a new WeatherPic", message: "", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Caption"
         }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Image URL or none"
+        }
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        let createQuotesAction = UIAlertAction(title: "Create", style: .default) { (action) in
+            let captionTextField = alertController.textFields![0]
+            let urlTextField = alertController.textFields![1]
+            let newPhotoBucket = Photo(context: self.context)
+            newPhotoBucket.caption = captionTextField.text
+            if(urlTextField.text == "") {
+                newPhotoBucket.url = "https://upload.wikimedia.org/wikipedia/commons/0/04/Hurricane_Isabel_from_ISS.jpg"
+            } else {
+                newPhotoBucket.url = urlTextField.text
+            }
+            newPhotoBucket.created = Date()
+//            self.photoBuckets.append(newPhotoBucket)
+            self.save()
+            self.updatePhotoBucketArray()
+            self.tableView.reloadData()
+            
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(createQuotesAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 
+    
+    func updatePhotoBucketArray() {
+        // make a fetch request
+        // execute the request in a try catch block
+        print("updating array")
+        let request: NSFetchRequest<Photo> = Photo.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "created", ascending: false)]
+        
+        do {
+            photoBuckets = try context.fetch(request)
+            print("photobuckets: \(photoBuckets)")
+        } catch {
+            fatalError("Unresolved Core Data Error")
+        }
+    }
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
+        if segue.identifier == "toDetailView" {
             if let indexPath = tableView.indexPathForSelectedRow {
-            let object = fetchedResultsController.object(at: indexPath)
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
+                (segue.destination as! DetailViewController).photo = photoBuckets[indexPath.row]
             }
         }
+    }
+    
+    func save() {
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
 
     // MARK: - Table View
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionInfo = fetchedResultsController.sections![section]
-        return sectionInfo.numberOfObjects
+        return photoBuckets.count
+//        return max(photoBuckets.count,1)
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
+        
+        cell.textLabel?.text = photoBuckets[indexPath.row].caption
         return cell
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return photoBuckets.count > 0
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let context = fetchedResultsController.managedObjectContext
-            context.delete(fetchedResultsController.object(at: indexPath))
-                
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+            context.delete(photoBuckets[indexPath.row])
+            self.save()
+            updatePhotoBucketArray()
+            
+            tableView.reloadData()
         }
     }
-
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
-    }
-
-    // MARK: - Fetched results controller
-
-    var fetchedResultsController: NSFetchedResultsController<Event> {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-        
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
-        
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-        
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
-        
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-        
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-             // Replace this implementation with code to handle the error appropriately.
-             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-             let nserror = error as NSError
-             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-        
-        return _fetchedResultsController!
-    }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
-
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-            case .insert:
-                tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-            case .delete:
-                tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-            default:
-                return
-        }
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-            case .insert:
-                tableView.insertRows(at: [newIndexPath!], with: .fade)
-            case .delete:
-                tableView.deleteRows(at: [indexPath!], with: .fade)
-            case .update:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
-            case .move:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
-                tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        }
-    }
-
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-
-    /*
-     // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
-     
-     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-         // In the simplest, most efficient, case, reload the table view.
-         tableView.reloadData()
-     }
-     */
-
 }
 
